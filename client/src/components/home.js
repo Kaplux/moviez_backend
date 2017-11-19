7a83a15230c3b0dde6fa94d921e8b72cd4e0e496
Alexandre Garbe <alex@MacBook-Pro-de-Alexandre.local>
2017-11-19 21:47:11 +0100

    Added « create new session » and « view past sessions » buttons

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { CurrentSessionContainer } from './session.js';
 import * as actionCreators from '../actions/sessionActionCreator.js';
+import { Button, Icon, Divider } from 'semantic-ui-react';
+
 
 class Home extends React.PureComponent {
 
@@ -9,8 +11,17 @@ class Home extends React.PureComponent {
 
         return (
             <div>
+                <Divider hidden />
+                <Button.Group >
+                    <Button size="big" positive><Icon name="add circle" />Create a new session</Button>
+                    <Button.Or />
+                    <Button size="big"><Icon name="history" />View past sessions</Button>
+                </Button.Group>
+                <Divider hidden />
                 <CurrentSessionContainer />
+
             </div>
+
         );
 
     }
